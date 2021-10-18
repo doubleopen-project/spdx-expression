702c1ca1911602e9d520a4c555b1c44e740f10fe
Mikko Murto <mikko.murto@hhpartners.fi>
2021-10-18 20:42:03 +0200

    feat: Add compound parsers
    Signed-off-by: Mikko Murto <mikko.murto@hhpartners.fi>

--- src/expression.rs
+++ src/expression.rs
@@ -1,8 +1,30 @@
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub(crate) enum SpdxExpression {
     SimpleExpression(SimpleExpression),
-    CompoundExpresion(CompoundExpression),
-    WithExpression,
+    CompoundExpression(CompoundExpression),
+    WithExpression(WithExpression),
+}
+
+impl SpdxExpression {
+    pub(crate) fn simple(
+        identifier: String,
+        document_ref: Option<String>,
+        license_ref: bool,
+    ) -> Self {
+        Self::SimpleExpression(SimpleExpression::new(identifier, document_ref, license_ref))
+    }
+
+    pub(crate) fn and(left: Self, right: Self) -> Self {
+        Self::CompoundExpression(CompoundExpression::new(left, SpdxOperator::And, right))
+    }
+
+    pub(crate) fn or(left: Self, right: Self) -> Self {
+        Self::CompoundExpression(CompoundExpression::new(left, SpdxOperator::Or, right))
+    }
+
+    pub(crate) fn with(license: SimpleExpression, exception: String) -> Self {
+        Self::WithExpression(WithExpression::new(license, exception))
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -33,4 +55,26 @@ pub(crate) struct CompoundExpression {
     pub(crate) left: Box<SpdxExpression>,
     pub(crate) operator: SpdxOperator,
     pub(crate) right: Box<SpdxExpression>,
+}
+
+impl CompoundExpression {
+    pub(crate) fn new(left: SpdxExpression, operator: SpdxOperator, right: SpdxExpression) -> Self {
+        Self {
+            left: Box::new(left),
+            operator,
+            right: Box::new(right),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub(crate) struct WithExpression {
+    pub(crate) license: SimpleExpression,
+    pub(crate) exception: String,
+}
+
+impl WithExpression {
+    pub(crate) fn new(license: SimpleExpression, exception: String) -> Self {
+        Self { license, exception }
+    }
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,17 +1,23 @@
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_while1},
-    character::streaming::char,
-    combinator::{complete, map, opt},
+    character::{complete::space1, streaming::char},
+    combinator::{complete, map, opt, recognize},
     error::VerboseError,
-    sequence::{delimited, separated_pair, terminated},
+    sequence::{delimited, pair, separated_pair, terminated, tuple},
     AsChar, IResult,
 };
 
-use crate::expression::SimpleExpression;
+use crate::expression::{
+    CompoundExpression, SimpleExpression, SpdxExpression, SpdxOperator, WithExpression,
+};
 
 fn idstring(i: &str) -> IResult<&str, &str, VerboseError<&str>> {
     take_while1(|c: char| c.is_alphanum() || c == '-' || c == '.')(i)
+}
+
+fn license_idstring(i: &str) -> IResult<&str, &str, VerboseError<&str>> {
+    recognize(pair(idstring, opt(complete(char('+')))))(i)
 }
 
 fn document_ref(i: &str) -> IResult<&str, &str, VerboseError<&str>> {
@@ -22,25 +28,88 @@ fn license_ref(i: &str) -> IResult<&str, (Option<&str>, &str), VerboseError<&str
     separated_pair(opt(document_ref), tag("LicenseRef-"), idstring)(i)
 }
 
-fn simple_expression(i: &str) -> IResult<&str, SimpleExpression, VerboseError<&str>> {
+fn simple_license_expression(i: &str) -> IResult<&str, SimpleExpression, VerboseError<&str>> {
     alt((
         map(license_ref, |(document_ref, id)| {
             let document_ref = document_ref.map(|document_ref| document_ref.to_string());
             SimpleExpression::new(id.to_string(), document_ref, true)
         }),
-        map(complete(terminated(idstring, char('+'))), |id| {
-            SimpleExpression::new(format!("{}+", id), None, false)
-        }),
-        map(idstring, |id| {
+        map(license_idstring, |id| {
             SimpleExpression::new(id.to_string(), None, false)
         }),
     ))(i)
 }
+
+fn simple_expression(i: &str) -> IResult<&str, SpdxExpression, VerboseError<&str>> {
+    alt((
+        map(license_ref, |(document_ref, id)| {
+            let document_ref = document_ref.map(|document_ref| document_ref.to_string());
+            SpdxExpression::simple(id.to_string(), document_ref, true)
+        }),
+        map(complete(terminated(idstring, char('+'))), |id| {
+            SpdxExpression::simple(format!("{}+", id), None, false)
+        }),
+        map(idstring, |id| {
+            SpdxExpression::simple(id.to_string(), None, false)
+        }),
+    ))(i)
+}
+
+fn with_expression(i: &str) -> IResult<&str, SpdxExpression, VerboseError<&str>> {
+    map(
+        tuple((simple_license_expression, space1, with, space1, idstring)),
+        |(license, _, _, _, exception)| SpdxExpression::with(license, exception.to_string()),
+    )(i)
+}
+
+fn and_expression(i: &str) -> IResult<&str, SpdxExpression, VerboseError<&str>> {
+    map(
+        tuple((
+            simple_expression,
+            space1,
+            and,
+            space1,
+            alt((and_expression, or_expression, simple_expression)),
+        )),
+        |(left, _, _, _, right)| SpdxExpression::and(left, right),
+    )(i)
+}
+
+fn or_expression(i: &str) -> IResult<&str, SpdxExpression, VerboseError<&str>> {
+    map(
+        tuple((
+            simple_expression,
+            space1,
+            or,
+            space1,
+            alt((and_expression, or_expression, simple_expression)),
+        )),
+        |(left, _, _, _, right)| SpdxExpression::or(left, right),
+    )(i)
+}
+
+fn with(i: &str) -> IResult<&str, &str, VerboseError<&str>> {
+    alt((tag("WITH"), tag("with")))(i)
+}
+
+fn and(i: &str) -> IResult<&str, &str, VerboseError<&str>> {
+    alt((tag("AND"), tag("and")))(i)
+}
+
+fn or(i: &str) -> IResult<&str, &str, VerboseError<&str>> {
+    alt((tag("OR"), tag("or")))(i)
+}
+
 #[cfg(test)]
 mod tests {
+    use pretty_assertions::assert_eq;
+
     use crate::{
-        expression::SimpleExpression,
-        parse::{document_ref, idstring, license_ref, simple_expression},
+        expression::{SimpleExpression, SpdxExpression},
+        parse::{
+            and_expression, document_ref, idstring, license_idstring, license_ref, or_expression,
+            simple_expression, with_expression,
+        },
     };
 
     #[test]
@@ -84,11 +153,7 @@ mod tests {
         let (_, result) = simple_expression("MIT").unwrap();
         assert_eq!(
             result,
-            SimpleExpression {
-                document_ref: None,
-                identifier: "MIT".to_string(),
-                license_ref: false
-            }
+            SpdxExpression::simple("MIT".to_string(), None, false)
         );
     }
 
@@ -97,11 +162,7 @@ mod tests {
         let (_, result) = simple_expression("LicenseRef-Unknown-license").unwrap();
         assert_eq!(
             result,
-            SimpleExpression {
-                document_ref: None,
-                identifier: "Unknown-license".to_string(),
-                license_ref: true
-            }
+            SpdxExpression::simple("Unknown-license".to_string(), None, true)
         );
     }
 
@@ -110,11 +171,7 @@ mod tests {
         let (_, result) = simple_expression("GPL-2.0+").unwrap();
         assert_eq!(
             result,
-            SimpleExpression {
-                document_ref: None,
-                identifier: "GPL-2.0+".to_string(),
-                license_ref: false
-            }
+            SpdxExpression::simple("GPL-2.0+".to_string(), None, false)
         );
     }
 
@@ -124,11 +181,119 @@ mod tests {
             simple_expression("DocumentRef-SPDX-DOC:LicenseRef-New-license-1.0").unwrap();
         assert_eq!(
             result,
-            SimpleExpression {
-                document_ref: Some("SPDX-DOC".to_string()),
-                identifier: "New-license-1.0".to_string(),
-                license_ref: true
-            }
+            SpdxExpression::simple(
+                "New-license-1.0".to_string(),
+                Some("SPDX-DOC".to_string()),
+                true
+            )
+        );
+    }
+
+    #[test]
+    fn license_idstring_without_plus() {
+        let (_, result) = license_idstring("GPL-2.0").unwrap();
+        assert_eq!(result, "GPL-2.0");
+    }
+
+    #[test]
+    fn license_idstring_with_plus() {
+        let (_, result) = license_idstring("GPL-2.0+").unwrap();
+        assert_eq!(result, "GPL-2.0+");
+    }
+
+    #[test]
+    fn with_expression_simple() {
+        let (_, result) = with_expression("GPL-2.0 WITH Autoconf-exception-2.0").unwrap();
+        assert_eq!(
+            result,
+            SpdxExpression::with(
+                SimpleExpression::new("GPL-2.0".to_string(), None, false),
+                "Autoconf-exception-2.0".to_string()
+            )
+        );
+    }
+
+    #[test]
+    fn and_expression_simple() {
+        let (_, result) = and_expression("GPL-2.0 AND MIT").unwrap();
+        assert_eq!(
+            result,
+            SpdxExpression::and(
+                SpdxExpression::simple("GPL-2.0".to_string(), None, false),
+                SpdxExpression::simple("MIT".to_string(), None, false),
+            )
+        );
+    }
+
+    #[test]
+    fn and_expression_nested() {
+        let (_, result) = and_expression("GPL-2.0 AND MIT AND LGPL-2.1").unwrap();
+        assert_eq!(
+            result,
+            SpdxExpression::and(
+                SpdxExpression::simple("GPL-2.0".to_string(), None, false),
+                SpdxExpression::and(
+                    SpdxExpression::simple("MIT".to_string(), None, false),
+                    SpdxExpression::simple("LGPL-2.1".to_string(), None, false)
+                )
+            )
+        );
+    }
+
+    #[test]
+    fn and_expression_nested_or() {
+        let (_, result) = and_expression("GPL-2.0 AND MIT OR LGPL-2.1").unwrap();
+        assert_eq!(
+            result,
+            SpdxExpression::and(
+                SpdxExpression::simple("GPL-2.0".to_string(), None, false),
+                SpdxExpression::or(
+                    SpdxExpression::simple("MIT".to_string(), None, false),
+                    SpdxExpression::simple("LGPL-2.1".to_string(), None, false)
+                )
+            )
+        );
+    }
+
+    #[test]
+    fn or_expression_simple() {
+        let (_, result) = or_expression("GPL-2.0 OR MIT").unwrap();
+        assert_eq!(
+            result,
+            SpdxExpression::or(
+                SpdxExpression::simple("GPL-2.0".to_string(), None, false),
+                SpdxExpression::simple("MIT".to_string(), None, false)
+            )
+        );
+    }
+
+    #[test]
+    fn or_expression_nested() {
+        let (_, result) = or_expression("GPL-2.0 OR MIT OR LGPL-2.1").unwrap();
+        assert_eq!(
+            result,
+            SpdxExpression::or(
+                SpdxExpression::simple("GPL-2.0".to_string(), None, false),
+                SpdxExpression::or(
+                    SpdxExpression::simple("MIT".to_string(), None, false),
+                    SpdxExpression::simple("LGPL-2.1".to_string(), None, false)
+                )
+            )
+        );
+    }
+
+    #[test]
+    fn or_expression_nested_and() {
+        let (_, result) = or_expression("GPL-2.0 OR MIT AND LGPL-2.1").unwrap();
+        assert_eq!(
+            result,
+            SpdxExpression::or(
+                SpdxExpression::simple("GPL-2.0".to_string(), None, false),
+                SpdxExpression::and(
+                    SpdxExpression::simple("MIT".to_string(), None, false),
+                    SpdxExpression::simple("LGPL-2.1".to_string(), None, false)
+                )
+            )
         );
     }
 }
